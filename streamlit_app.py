a7634e54d1e41d744447ac88d4d27666c94447e7
IndiraDewi29 <indira.d.savitry@gmail.com>
2025-05-28 08:00:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title("🛴Welcome to Indira's webstite!")
 st.write("Ayo brunch sushi sambil jalan-jalan di paskal!")
-s.image("IMG_0335.jpeg", width=200)
+st.image("IMG_0335.jpeg", width=200)
