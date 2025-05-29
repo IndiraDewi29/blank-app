a73e1556ca83f3d202dd687221af3e4eecb32b61
IndiraDewi29 <indira.d.savitry@gmail.com>
2025-05-30 00:28:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,13 @@ import streamlit as st
 st.title("🛴Welcome to Indira's webstite!")
 st.write("Ayo brunch sushi sambil jalan-jalan di paskal!")
 st.image("IMG_0335.jpeg", width=200)
+###########
+st.title("Aplikasi sederhana")
+c1,c2 = st.columns(2)
+with c1:
+    st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+    angka = st.number_input("Tulis Sebuah Angka:", value=0, step=1)
+    if (angka % 2) == 0:
+       st.write(f"{angka} adlah bilangan genap")
+     else :
+       st.write(f"{angka} adalah bilangan ganjil")
