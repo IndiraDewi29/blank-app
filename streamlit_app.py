681b70e8c91f3b775f3d7deb2bd070a09fa8399b
IndiraDewi29 <indira.d.savitry@gmail.com>
2025-06-02 14:02:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,6 @@ with c1:
     st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
     angka = st.number_input("Tulis Sebuah Angka:", value=0, step=1)
     if (angka % 2) == 0:
-       st.write(f"{angka} adlah bilangan genap")
+       st.write(f"{angka} adalah bilangan genap")
      else :
        st.write(f"{angka} adalah bilangan ganjil")
